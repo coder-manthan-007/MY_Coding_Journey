e634949dbd9cb559b003e4394629a21c3227f584
Manthan Devi <manthanmdevi@gmail.com>
2026-08-20 13:32:12 +0530

    Time: 0 ms (100%), Space: 17.3 MB (38.28%) - CodeHub

---
+++ leetcode/2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int cnt = 0;
+        for(int i = 0;i<details.size();i++){
+            if(details[i][11]-'0'>6)cnt++;
+            else if(details[i][11]-'0'==6 && details[i][12]-'0'>0)cnt++;
+        }
+        return cnt;
+    }
+};
